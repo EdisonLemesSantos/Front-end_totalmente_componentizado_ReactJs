8d763fefe648c4264e0941d1d6c475293fd53500
EdisonLemesSantos <edisonlemessantos@gmail.com>
2022-07-01 17:03:19 -0300

    Criando commit pelo visual Code, aula 4

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -4,14 +4,15 @@ import * as S from "./styled";
 const Repositories = () => {
     return (
         <S.WrapperTabs
-        selectedTabClassName="is-selected"
-        selectedTabPanelClassName="is-selected"
+            selectedTabClassName="is-selected"
+            selectedTabPanelClassName="is-selected"
         >
             <S.WrapperTabList>
                 <S.WrapperTab>Repositories</S.WrapperTab>
                 <S.WrapperTab>Starred</S.WrapperTab>
             </S.WrapperTabList>   
-
+                <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
+                <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
         </S.WrapperTabs>
     );
 };
